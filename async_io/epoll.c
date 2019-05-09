e53bb5373e530dd20e09a3f0fe7e431292b1a76c
chenlichun <razertory@163.com>
2019-05-09 13:48:55 +0800

    4 space tab size

--- async_io/epoll.c
+++ async_io/epoll.c
@@ -13,83 +13,83 @@
 
 void child_process(void)
 {
-  sleep(1);
-  char msg[MAXBUF];
-  struct sockaddr_in addr = {0};
-  int n, sockfd, num = 1;
-  srandom(getpid());
+    sleep(1);
+    char msg[MAXBUF];
+    struct sockaddr_in addr = {0};
+    int n, sockfd, num = 1;
+    srandom(getpid());
 
-  /* 创建 socket */
-  sockfd = socket(AF_INET, SOCK_STREAM, 0);
-  addr.sin_family = AF_INET;
-  addr.sin_port = htons(2000);
-  addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    /* 创建 socket */
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(2000);
+    addr.sin_addr.s_addr = inet_addr("127.0.0.1");
 
-  /* 连接到 server */
-  connect(sockfd, (struct sockaddr *)&addr, sizeof(addr));
-  printf("client {%d} connected \n", getpid());
+    /* 连接到 server */
+    connect(sockfd, (struct sockaddr *)&addr, sizeof(addr));
+    printf("client {%d} connected \n", getpid());
 
-  /* 往 server 写数据 */
-  while (1)
-  {
-    int sl = (random() % 10) + 1;
-    num++;
-    sleep(sl);
-    sprintf(msg, "Test message %d from client %d", num, getpid());
-    n = write(sockfd, msg, strlen(msg)); /* Send message */
-  }
+    /* 往 server 写数据 */
+    while (1)
+    {
+        int sl = (random() % 10) + 1;
+        num++;
+        sleep(sl);
+        sprintf(msg, "Test message %d from client %d", num, getpid());
+        n = write(sockfd, msg, strlen(msg)); /* Send message */
+    }
 }
 
 int main()
 {
-  char buffer[MAXBUF];
-  int fds[5];
-  struct epoll_event events[5];
-  int epfd = epoll_create(10);
+    char buffer[MAXBUF];
+    int fds[5];
+    struct epoll_event events[5];
+    int epfd = epoll_create(10);
 
-  struct sockaddr_in addr;
-  struct sockaddr_in client;
-  int addrlen, n, i, max = 0;
-  ;
-  int sockfd, commfd;
-  fd_set rset;
-  for (i = 0; i < 5; i++)
-  {
-    if (fork() == 0)
+    struct sockaddr_in addr;
+    struct sockaddr_in client;
+    int addrlen, n, i, max = 0;
+    ;
+    int sockfd, commfd;
+    fd_set rset;
+    for (i = 0; i < 5; i++)
     {
-      child_process();
-      exit(0);
+        if (fork() == 0)
+        {
+            child_process();
+            exit(0);
+        }
     }
-  }
 
-  sockfd = socket(AF_INET, SOCK_STREAM, 0);
-  memset(&addr, 0, sizeof(addr));
-  addr.sin_family = AF_INET;
-  addr.sin_port = htons(2000);
-  addr.sin_addr.s_addr = INADDR_ANY;
-  bind(sockfd, (struct sockaddr *)&addr, sizeof(addr));
-  listen(sockfd, 5);
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    memset(&addr, 0, sizeof(addr));
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(2000);
+    addr.sin_addr.s_addr = INADDR_ANY;
+    bind(sockfd, (struct sockaddr *)&addr, sizeof(addr));
+    listen(sockfd, 5);
 
-  for (i = 0; i < 5; i++)
-  {
-    static struct epoll_event ev;
-    memset(&client, 0, sizeof(client));
-    addrlen = sizeof(client);
-    ev.data.fd = accept(sockfd, (struct sockaddr *)&client, &addrlen);
-    ev.events = EPOLLIN;
-    epoll_ctl(epfd, EPOLL_CTL_ADD, ev.data.fd, &ev);
-  }
-
-  while (1)
-  {
-    puts("round again");
-    int nfds = epoll_wait(epfd, events, 5, 10000);
-    for (i = 0; i < nfds; i++)
+    for (i = 0; i < 5; i++)
     {
-      memset(buffer, 0, MAXBUF);
-      read(events[i].data.fd, buffer, MAXBUF);
-      puts(buffer);
+        static struct epoll_event ev;
+        memset(&client, 0, sizeof(client));
+        addrlen = sizeof(client);
+        ev.data.fd = accept(sockfd, (struct sockaddr *)&client, &addrlen);
+        ev.events = EPOLLIN;
+        epoll_ctl(epfd, EPOLL_CTL_ADD, ev.data.fd, &ev);
     }
-  }
-  return 0;
+
+    while (1)
+    {
+        puts("round again");
+        int nfds = epoll_wait(epfd, events, 5, 10000);
+        for (i = 0; i < nfds; i++)
+        {
+            memset(buffer, 0, MAXBUF);
+            read(events[i].data.fd, buffer, MAXBUF);
+            puts(buffer);
+        }
+    }
+    return 0;
 }
